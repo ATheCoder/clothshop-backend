f95f4acee78a5d5d80638e2ea6e040b24db37d98
TheGreatA <nervous.fiend@gmail.com>
2018-07-07 23:21:42 +0430

    getClothes now gives Categories too.

--- controllers/getAllClothes.js
+++ controllers/getAllClothes.js
@@ -2,7 +2,7 @@ const abstractClothModel = require('../models/AbstractClothModel')
 
 module.exports = async () => {
   try{
-    return await abstractClothModel.find()
+    return await abstractClothModel.find().populate('category')
   }
   catch(e){
     throw e
