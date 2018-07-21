b1768424e1dfb6a0ae0a7b2460a256ab66d5cbbc
ATheCoder <nervous.fiend@gmail.com>
2018-07-21 21:09:54 +0430

    Fixed Color not populating for ID search

--- cloth/getClothByID.js
+++ cloth/getClothByID.js
@@ -4,5 +4,8 @@ module.exports = async (id) => {
   return await clothModel.find({abstract: id}).populate('color').populate('abstract').populate({
     path: 'abstract',
     populate: {path: 'category'}
+  }).populate({
+    path: 'abstract',
+    populate: {path: 'color'}
   })
 }
